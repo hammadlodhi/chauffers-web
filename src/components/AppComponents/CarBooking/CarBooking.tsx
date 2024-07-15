bb5dcd4df9f82c4d0e014e7a3251e62f54dab72e
hammad lodhi <hammadak05@gmail.com>
2024-07-15 19:10:43 +0500

    Get a Quote query added

--- src/components/AppComponents/CarBooking/CarBooking.tsx
+++ src/components/AppComponents/CarBooking/CarBooking.tsx
@@ -1,25 +1,53 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaGlobe, FaPhoneAlt } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import { useForm, SubmitHandler } from "react-hook-form";
 import ErrorMessage from "@/components/UIComponents/ErrorMessage/ErrorMessage";
 import { Button, FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 interface IBookCarInput {
-  pickup_address: string;
-  destination_address: string;
-  car_type: string;
-  journey_type: string;
   name: string;
   email: string;
   phone: string;
+  pickup_address: string;
+  destination_address: string;
+  hours_of_waiting: number;
+  return_pickup_address?: string;
+  return_destination_address?: string;
+  car_type: string;
+  journey_type: string;
   date: string;
   time: string;
   description: string;
 }
 
 const CarBooking = () => {
+  const [returnJourneyType, setReturnJourneyType] = useState(false);
+  const [showWaitingHours, setShowWaitingHours] = useState(false);
+  const [heading, setHeading] = useState("");
+  const router = useRouter();
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const quote = params.get("quote");
+    if (quote === "single-journey") {
+      setHeading("Single Journey");
+      setShowWaitingHours(false);
+    } else {
+      setHeading("Chauffeur Service");
+      setShowWaitingHours(true);
+    }
+  }, [router]);
+  const handleJourneyTypeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.value === "Return Journey") {
+      setReturnJourneyType(true);
+    } else {
+      setReturnJourneyType(false);
+    }
+  };
   const {
     register,
     formState: { errors },
@@ -54,18 +82,19 @@ const CarBooking = () => {
         </div>
       </div>
       <div className="car-booking__contact-form">
-        <h1>Get A Quote</h1>
-        <p>Book a car by filling up the form below</p>
+        <h1 className="mg-b-10">{heading}</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
-        <input
+          <label htmlFor="name">Name</label>
+          <input
             type="text"
             placeholder="Enter your name"
             {...register("name", { required: "Name is required" })}
           />
           <ErrorMessage text={errors.name?.message} />
+          <label htmlFor="email">Email Address</label>
           <input
             type="text"
-            placeholder="Email"
+            placeholder="Enter your email"
             {...register("email", {
               required: "Email is required",
               pattern: {
@@ -75,33 +104,21 @@ const CarBooking = () => {
             })}
           />
           <ErrorMessage text={errors.email?.message} />
+          <label htmlFor="phone">Phone number</label>
           <input
             type="text"
-            placeholder="Phone"
+            placeholder="Enter your phone number"
             {...register("phone", { required: "Phone is required" })}
           />
           <ErrorMessage text={errors.phone?.message} />
-          <input
-            type="text"
-            placeholder="Pickup Address"
-            {...register("pickup_address", {
-              required: "Pickup address is required",
-            })}
-          />
-          <ErrorMessage text={errors.pickup_address?.message} />
-          <input
-            type="text"
-            placeholder="Destination Address"
-            {...register("destination_address", {
-              required: "Destination address is required",
-            })}
-          />
-          <ErrorMessage text={errors.destination_address?.message} />
+          <label htmlFor="car_type">Car type</label>
           <select
             className="car-booking__select-box"
             {...register("car_type", { required: "Car type is required" })}
           >
-            <option value="" disabled selected>Select your car type</option>
+            <option value="" disabled selected>
+              Select your car type
+            </option>
             <option value="1">Mercedes S-Class</option>
             <option value="2">Mercedes V-Class</option>
             <option value="4">Range Rover</option>
@@ -109,12 +126,54 @@ const CarBooking = () => {
             <option value="3">Rolls Royce Phantom</option>
           </select>
           <ErrorMessage text={errors.car_type?.message} />
+          <label htmlFor="pickup_address">Pickup address</label>
+          <input
+            type="text"
+            placeholder="Enter your pickup address"
+            {...register("pickup_address", {
+              required: "Pickup address is required",
+            })}
+          />
+          <ErrorMessage text={errors.pickup_address?.message} />
+          <label htmlFor="destination_address">Destination address</label>
+          <input
+            type="text"
+            placeholder="Enter your destination address"
+            {...register("destination_address", {
+              required: "Destination address is required",
+            })}
+          />
+          <ErrorMessage text={errors.destination_address?.message} />
+          {showWaitingHours && (
+            <>
+              <label htmlFor="car_type">Hours of Waiting</label>
+              <select
+                className="car-booking__select-box"
+                {...register("hours_of_waiting", {
+                  required: "Hours of waiting is required",
+                })}
+              >
+                <option value="" disabled selected>
+                  Select your hours of waiting
+                </option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="4">3</option>
+                <option value="4">4</option>
+                <option value="3">5</option>
+                <option value="3">More</option>
+              </select>
+              <ErrorMessage text={errors.hours_of_waiting?.message} />
+            </>
+          )}
+          <label htmlFor="journey_type">Journey type</label>
           <RadioGroup
             row
             aria-labelledby="demo-row-radio-buttons-group-label"
             {...register("journey_type", {
               required: "Journey type is required",
             })}
+            onChange={handleJourneyTypeChange}
           >
             <FormControlLabel
               value="Single Journey"
@@ -140,22 +199,35 @@ const CarBooking = () => {
               }
               label="Return Journey"
             />
-            <FormControlLabel
-              value="Multi-Destination Journey"
-              control={
-                <Radio
-                  sx={{
-                    color: "#dac06c",
-                    "&.Mui-checked": { color: "#dac06c" },
-                  }}
-                />
-              }
-              label="Multi-Destination Journey"
-            />
           </RadioGroup>
           <ErrorMessage text={errors.journey_type?.message} />
-          <input type="datetime-local" placeholder="Time to pickup" />
+          {returnJourneyType && (
+            <>
+              <label htmlFor="return_pickup_address">
+                Return pickup address
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your return pickup address"
+                {...register("return_pickup_address")}
+              />
+              <label htmlFor="destination_address">
+                Return destination address
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your return destination address"
+                {...register("return_destination_address")}
+              />
+            </>
+          )}
+          <label htmlFor="pickup_time">Time to pickup</label>
+          <input type="datetime-local" />
           <ErrorMessage text={errors.time?.message} />
+          <label htmlFor="destination_time">Time to dropoff</label>
+          <input type="datetime-local" />
+          <ErrorMessage text={errors.time?.message} />
+          <label htmlFor="description">Description</label>
           <textarea
             placeholder="Enter Additional Notes"
             rows={10}
--- src/components/AppComponents/GetAQuote/GetAQuote.tsx
+++ src/components/AppComponents/GetAQuote/GetAQuote.tsx
@@ -1,46 +1,60 @@
-import { car1, car10, car9, cover } from '@/util/images';
-import { bookCarRoute, homeRoute } from '@/util/routes';
-import Link from 'next/link';
-import React from 'react';
+"use client"
+
+import { car1, car10, car9, cover } from "@/util/images";
+import { bookCarRoute, homeRoute } from "@/util/routes";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const GetAQuote = () => {
+  const router = useRouter();
   return (
-    <div className='layout-width get-a-quote'>
+    <div className="layout-width get-a-quote">
       <div>
         <img
           width={1200}
           src={cover}
-          alt=''
-          className='get-a-quote__responsive-image'
+          alt=""
+          className="get-a-quote__responsive-image"
         />
       </div>
-      <h1 className='regions mg-t-15 white'>Get A Quote</h1>
+      <h1 className="regions mg-t-15 white">Get A Quote</h1>
       <hr />
-      <div className='get-a-quote__img'>
-        <div className='get-a-quote__image-container'>
-          <Link href={bookCarRoute}>
+      <div className="get-a-quote__img">
+        <div className="get-a-quote__image-container">
+          <Link
+            href={{
+              pathname: bookCarRoute,
+              query: { quote: "single-journey" },
+            }}
+          >
             <img
               // width={420}
               src={car9}
-              alt=''
-              className='get-a-quote__images'
+              alt=""
+              className="get-a-quote__images"
             />
           </Link>
-          <div className='get-a-quote__centered-text'>Single Journey</div>
+          <div className="get-a-quote__centered-text">Single Journey</div>
         </div>
-        <div className='get-a-quote__image-container'>
-          <Link href={bookCarRoute}>
+        <div className="get-a-quote__image-container">
+          <Link
+            href={{
+              pathname: bookCarRoute,
+              query: { quote: "chauffeur-service" },
+            }}
+          >
             <img
               // width={420}
               src={car10}
-              alt=''
-              className='get-a-quote__img2 get-a-quote__images'
+              alt=""
+              className="get-a-quote__img2 get-a-quote__images"
             />
           </Link>
-          <div className='get-a-quote__centered-text'>Chauffeur</div>
+          <div className="get-a-quote__centered-text">Chauffeur Service</div>
         </div>
       </div>
-      <hr className='mg-t-20' />
+      <hr className="mg-t-20" />
     </div>
   );
 };
